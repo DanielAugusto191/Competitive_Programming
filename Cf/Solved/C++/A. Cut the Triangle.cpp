a02fabf08026aba583ebce2f4d81ce95461ff5b0
DanielAugusto191 <daniel.augusto191@gmail.com>
2023-01-07 18:11:30 -0300

    +1

--- Cf/Solved/C++/A. Cut the Triangle.cpp
+++ Cf/Solved/C++/A. Cut the Triangle.cpp
@@ -1,4 +1,4 @@
-// 
+// https://codeforces.com/contest/1767/problem/A
 #include <bits/stdc++.h>
 using namespace std;
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
@@ -7,6 +7,8 @@ using namespace std;
 #define dbg(a) cout << a << " -- DEBUG -- "<< endl;
 #define Adbg(a) for(auto e: a){cout << e << ' ';};cout << endl;
 #define Mdbg(a) for(auto e: a){cout << e.first << ' ' <<  e.second;cout << endl;}
+#define f first
+#define s second
 
 typedef long long ll;
 typedef pair<int,int> pii;
@@ -17,6 +19,15 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 const int MAX = 1e9;
 
 int main(){_
-
+    int t;cin>>t;
+    while(t--){
+        string tt;getline(cin, tt);
+        unordered_set<int> x,y;
+        for(int i=0;i<3;++i){
+            int a,b;cin>>a>>b;
+            x.insert(a);y.insert(b);
+        }
+        cout << ((x.size() == 3 || y.size() ==3)?"YES":"NO") << endl;
+    }
 	exit(0);
 }
